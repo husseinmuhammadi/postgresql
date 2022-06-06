1901e2500af9dc1120060c461e957209aee4d860
Hossein Mohammadi <hmohammadi@mobiquityinc.com>
2022-06-06 12:00:53 +0200

    database connection

---
+++ postgresql-database-connection/src/test/java/com/javastudio/postgresql/ConnectionTest.java
@@ -0,0 +1,52 @@
+package com.javastudio.postgresql;
+
+import org.junit.jupiter.api.*;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.sql.*;
+import java.util.Properties;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ConnectionTest {
+
+    static Properties properties = new Properties();
+
+    @BeforeAll
+    static void beforeAll() throws IOException {
+        try (InputStream in = ConnectionTest.class.getClassLoader().getResourceAsStream("datasource.properties")) {
+            properties.load(in);
+        }
+    }
+
+    @BeforeEach
+    void setUp() throws IOException {
+
+    }
+
+    @Test
+    void shouldLoadPostgresqlDriverClass() throws ClassNotFoundException {
+        Class<?> driver = Class.forName(properties.getProperty("driver-class-name"));
+        Assertions.assertEquals("org.postgresql.Driver", driver.getName());
+    }
+
+    @Test
+    void shouldConnectAndRunQuery() throws SQLException, ClassNotFoundException {
+
+        Class.forName(properties.getProperty("driver-class-name"));
+
+        Connection connection = DriverManager.getConnection(
+                properties.getProperty("url"),
+                properties.getProperty("username"),
+                properties.getProperty("password")
+        );
+
+        assertTrue(connection.getAutoCommit());
+
+        Statement statement = connection.createStatement();
+        ResultSet resultSet = statement.executeQuery("select 1");
+        assertTrue(resultSet.next());
+        assertEquals(1, resultSet.getInt(1));
+    }
+}
